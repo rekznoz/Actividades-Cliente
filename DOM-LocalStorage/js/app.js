c2416e91d91972fdb6acc2b1adb2c5982647698d
RekZNoZ <rafikicroz@hotmail.com>
2024-10-24 17:33:55 +0200

    LocalStorage

--- DOM-LocalStorage/js/app.js
+++ DOM-LocalStorage/js/app.js
@@ -3,10 +3,21 @@ const listaTweets = document.querySelector('#lista-tweets')
 
 let tweets = []
 
+// Local Storage
+document.addEventListener('DOMContentLoaded', () => {
+    tweets = JSON.parse(localStorage.getItem('tweets')) || []
+    mostrarTweets()
+})
+
 function publicarTweet(e) {
     e.preventDefault()
 
     const tweet = document.querySelector('#tweet').value
+
+    if (tweet.trim() === '') {
+        alert('El campo no puede estar vacio')
+        return
+    }
 
     const tweetObj = {
         id: Date.now(),
@@ -15,15 +26,45 @@ function publicarTweet(e) {
 
     tweets = [...tweets, tweetObj]
 
-    mostrarTweet(tweetObj)
+    guardarStorage()
+
+    mostrarTweets()
 
     formulario.reset()
 
 }
 formulario.addEventListener('submit', publicarTweet)
 
-function mostrarTweet(tweet) {
-    const li = document.createElement('li')
-    li.innerText = tweet.tweet
-    listaTweets.appendChild(li)
+function mostrarTweets() {
+
+    while (listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild)
+    }
+
+    tweets.forEach(tweet => {
+        const li = document.createElement('li')
+        li.innerText = tweet.tweet
+
+        const btnBorrar = document.createElement('button')
+        btnBorrar.innerText = 'X'
+        btnBorrar.classList.add('borrar-tweet')
+
+        btnBorrar.onclick = () => {
+            borrarTweet(tweet.id)
+        }
+
+        li.appendChild(btnBorrar)
+        listaTweets.appendChild(li)
+    })
+
+}
+
+function guardarStorage() {
+    localStorage.setItem('tweets', JSON.stringify(tweets))
+}
+
+function borrarTweet(id) {
+    tweets = tweets.filter(tweet => tweet.id !== id)
+    guardarStorage()
+    mostrarTweets()
 }
