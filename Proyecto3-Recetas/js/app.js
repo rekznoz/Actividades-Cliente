ad34cc06fe8d4523fbaa9ebacca858a845b401d2
RekZNoZ <rafikicroz@hotmail.com>
2024-11-02 17:27:25 +0100

    Javascript basico

--- Proyecto3-Recetas/js/app.js
+++ Proyecto3-Recetas/js/app.js
@@ -7,7 +7,16 @@
 // Filtrar categorias
 // www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
 
+// Receta por ID
+// www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
+// Buscar receta
+// www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+
 const categorias = document.querySelector('#categorias')
+const cuerpoRecetas = document.querySelector('#resultado')
+const modal = document.querySelector('#modal')
+const modalBody = document.querySelector('#modal .modal-content')
 
 function cargarCategorias (){
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
@@ -37,10 +46,201 @@ function filtrarPorCategoria(evento){
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
-            console.log(datos)
+            mostrarRecetas(datos)
         })
         .catch(error => {
             console.log(error)
         })
 }
 categorias.addEventListener('change', filtrarPorCategoria)
+
+function mostrarRecetas(datos){
+
+    limpiarHTML(cuerpoRecetas)
+
+    const {meals} = datos
+    meals.forEach(receta => {
+        const {strMeal, strMealThumb, idMeal} = receta
+        const div = document.createElement('div')
+        div.classList.add('col-md-4', 'col-sm-6', 'col-12')
+
+        const divCard = document.createElement('div')
+        divCard.classList.add('card', 'mb-4', 'shadow-sm', 'rounded')
+        div.appendChild(divCard)
+
+        const img = document.createElement('img')
+        img.src = strMealThumb
+        img.alt = strMeal
+        img.classList.add('card-img-top', 'img-fluid', 'rounded', 'mx-auto', 'd-block', 'my-3')
+        divCard.appendChild(img)
+
+        const divCardBody = document.createElement('div')
+        divCardBody.classList.add('card-body')
+        divCard.appendChild(divCardBody)
+
+        const h2 = document.createElement('h2')
+        h2.textContent = strMeal
+        h2.classList.add('card-title', 'text-center', 'my-3')
+        divCardBody.appendChild(h2)
+
+        const a = document.createElement('a')
+        a.href = '#'
+        a.classList.add('btn', 'btn-primary', 'd-block', 'mx-auto', 'my-3', 'botonVer')
+        a.dataset.toggle = 'modal'
+        a.dataset.target = '#receta'
+        a.dataset.id = idMeal
+        a.textContent = 'Ver receta'
+        divCardBody.appendChild(a)
+
+        cuerpoRecetas.appendChild(div)
+    })
+}
+
+function mostrarReceta(evento){
+    evento.preventDefault()
+    if(evento.target.classList.contains('botonVer')){
+        console.log('Ver receta')
+        const id = evento.target.dataset.id
+        console.log(id)
+        const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(receta => {
+                mostrarModal(receta)
+                console.log(receta)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+}
+cuerpoRecetas.addEventListener('click', mostrarReceta)
+
+function mostrarModal(datos){
+
+    limpiarHTML(modalBody)
+
+    const header = document.createElement('div')
+    header.classList.add('modal-header')
+
+    const h1 = document.createElement('h1')
+    h1.classList.add('modal-title', 'fs-3', 'font-bold')
+    h1.id = 'staticBackdropLabel'
+
+    const button = document.createElement('button')
+    button.type = 'button'
+    button.classList.add('btn-close')
+    button.dataset.bsDismiss = 'modal'
+    button.ariaLabel = 'Close'
+
+    header.appendChild(h1)
+    header.appendChild(button)
+
+    const cuerpo = document.createElement('div')
+    cuerpo.classList.add('modal-body')
+
+    const footer = document.createElement('div')
+    footer.classList.add('modal-footer', 'flex', 'justify-content-between')
+
+    modalBody.appendChild(header)
+    modalBody.appendChild(cuerpo)
+    modalBody.appendChild(footer)
+
+    modal.classList.add('show')
+    modal.style.display = 'block'
+
+    const {meals} = datos
+
+    const {strMeal, strMealThumb, strInstructions, strYoutube} = meals[0]
+
+    const ingredientes = []
+    for(let i = 1; i <= 20; i++){
+        if(meals[0][`strIngredient${i}`]){
+            ingredientes.push(meals[0][`strIngredient${i}`])
+        }
+    }
+
+    const medidas = []
+    for(let i = 1; i <= 20; i++){
+        if(meals[0][`strMeasure${i}`]){
+            medidas.push(meals[0][`strMeasure${i}`])
+        }
+    }
+
+    h1.textContent = strMeal
+
+    const img = document.createElement('img')
+    img.src = strMealThumb
+    img.alt = strMeal
+    img.classList.add('img-fluid', 'rounded', 'mx-auto', 'd-block', 'my-3')
+    cuerpo.appendChild(img)
+
+    const p = document.createElement('p')
+    p.textContent = strInstructions
+
+    const h3 = document.createElement('h3')
+    h3.textContent = 'Ingredientes'
+    h3.classList.add('my-3', 'text-center')
+
+    const ul = document.createElement('ul')
+    ul.classList.add('list-group')
+
+    ingredientes.forEach((ingrediente, index) => {
+        const li = document.createElement('li')
+        li.classList.add('list-group-item')
+        li.textContent = `${ingrediente} - ${medidas[index]}`
+        ul.appendChild(li)
+    })
+
+    const h3Video = document.createElement('h3')
+    h3Video.textContent = 'Video Receta'
+    h3Video.classList.add('my-3', 'text-center')
+
+    const a = document.createElement('a')
+    a.href = strYoutube
+    a.target = '_blank'
+    a.classList.add('btn', 'btn-primary', 'd-block', 'mx-auto', 'my-3')
+    a.textContent = 'Ver video'
+
+    cuerpo.appendChild(p)
+    cuerpo.appendChild(h3)
+    cuerpo.appendChild(ul)
+    cuerpo.appendChild(h3Video)
+    cuerpo.appendChild(a)
+
+    button.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
+
+    window.addEventListener('click', (evento) => {
+        if(evento.target === modal){
+            modal.style.display = 'none'
+        }
+    })
+
+    // Boton cerrar y AgregarFavoritos en Footer
+
+    const buttonCerrar = document.createElement('button')
+    buttonCerrar.classList.add('btn', 'btn-secondary', 'col')
+    buttonCerrar.textContent = 'Cerrar'
+    buttonCerrar.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
+
+    const buttonFavoritos = document.createElement('button')
+    buttonFavoritos.classList.add('btn', 'btn-primary', 'col')
+    buttonFavoritos.textContent = 'Agregar a Favoritos'
+    buttonFavoritos.addEventListener('click', () => {
+        console.log('Agregar a Favoritos')
+    })
+
+    footer.appendChild(buttonCerrar)
+    footer.appendChild(buttonFavoritos)
+
+}
+
+function limpiarHTML(padre){
+    while(padre.firstChild){
+        padre.removeChild(padre.firstChild)
+    }
+}
